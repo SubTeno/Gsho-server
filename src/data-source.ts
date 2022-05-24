3b38ea4496d2330f6bfb56788b57b48420786044
SubTeno <elppa221@protonmail.com>
2022-05-25 01:40:32 +0700

    testing

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,10 +1,10 @@
-import { gloss } from './graphql/Entities/gloss';
-import { sense } from './graphql/Entities/sense';
-import { reading } from './graphql/Entities/reading';
-import { kanji_code } from './graphql/Entities/kanji_code';
-import { kanji } from './graphql/Entities/kanji';
+import { gloss } from "./graphql/Entities/gloss";
+import { sense } from "./graphql/Entities/sense";
+import { reading } from "./graphql/Entities/reading";
+import { kanji_code } from "./graphql/Entities/kanji_code";
+import { kanji } from "./graphql/Entities/kanji";
 import { DataSource } from "typeorm";
-import entry from './graphql/Entities/entry';
+import entry from "./graphql/Entities/entry";
 
 // Data Source
 export const AppDataSource = new DataSource({
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
   database: "./jmdict.sqlite3",
   synchronize: false,
   logging: false,
-  cache:true,
-  entities: [kanji,kanji_code,reading,sense,entry,gloss],
-  relationLoadStrategy:"query"
+  cache: true,
+  entities: [kanji, kanji_code, reading, sense, entry, gloss],
+  relationLoadStrategy: "query",
 });
